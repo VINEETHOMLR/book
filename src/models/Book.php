a15d0cbba77d10ea98d821ff1db13d6b3b9837bb
Vineeth <ipcs022@outlook.com>
2021-08-29 18:03:29 +0530

    book list

---
+++ src/models/Book.php
@@ -0,0 +1,144 @@
+<?php
+
+
+namespace src\models;
+
+use inc\Raise;
+use src\lib\Database;
+use src\lib\Router;
+use src\traits\DataTableTrait;
+use src\traits\FilterTrait;
+use src\traits\ModelTrait;
+use src\lib\Helper as H;
+
+
+class Book extends Database
+{
+    use ModelTrait, FilterTrait, DataTableTrait;
+    protected $pk = 'id';
+    /**
+     * Constructor of the model
+     */
+    public function __construct($db = "db")
+    {
+        
+        parent::__construct(Raise::db()[$db]);
+
+        $this->tableName = "book";
+
+        $this->columns = [
+            'id', 
+            'title', 
+            'message',
+            'createtime',  
+        ];
+    }
+
+    /**
+     *
+     * @return Array
+     */
+    public static function attrs()
+    {
+        return  [
+            'id', 
+            'title', 
+            'message', 
+            'createtime', 
+        ];
+    }
+
+    /**
+     *
+     * @return $this
+     */
+    public function assignAttrs($attrs = [])
+    {   
+        $isExternal = !empty($attrs);
+        foreach (($isExternal ? $attrs : self::attrs()) as $eAttr => $attr) {
+            $aAttr = $isExternal ? $eAttr : $attr;
+           $this->{$aAttr} = $isExternal ? $attr : "";
+        }
+        
+        return $this;
+    }
+
+
+    /**
+     *
+     * @param INT $pk
+     */
+    public function findByPK($pk)
+    {
+        $dtAry = parent::findByPK($pk);
+        foreach ($dtAry as $attr => $val) {
+            $this->{$attr} = $val;
+        }
+        return $this;
+    }
+
+   
+    /**
+     *
+     * @return attrs data array
+     */
+    public function convertArray()
+    {
+        $temp = array();
+        $attrs = $this->attrs();
+        foreach ($attrs as $key) {
+            $temp[$key] = isset($this->{$key})?$this->{$key}:'';
+        }
+        return $temp;
+    }
+
+    public function createRecord($data)
+    {
+        $this->assignAttrs($data);
+        return $this->save();
+    }    
+
+
+    public function getList($params){
+
+
+        $where = " WHERE status=1 ";
+        if(!empty($params['category_id'])) {
+            
+            $where.= " AND category_id=$params[category_id]";
+        }
+        $response = $this->callSql("SELECT * FROM $this->tableName $where  ORDER BY id DESC ","rows");
+
+
+        $rows = array();
+
+        if (!empty($response)) {
+                foreach ($response as $key => $info) {
+                    
+                    $author_id  = $info['user_id'];
+                    $author = $this->callSql("SELECT fullname FROM user WHERE id=$author_id","value");
+                   
+                    $rows[$key]['id']    = !empty($info['id'])?strval($info['id']):'-';
+                    $rows[$key]['title']  = !empty($info['title'])?$info['title']:'-';
+                    $rows[$key]['author']  = !empty($author)?$author:'-';
+                    $rows[$key]['cover_photo'] = !empty($info['cover_photo'])?BASEURL.'web/uploads/cover/'.$info['cover_photo']:'';
+                    $rows[$key]['synopsis']  = !empty($info['synopsis'])?$info['synopsis']:'-';
+                    
+                }
+        }
+
+       
+
+        
+
+        return !empty($rows) ? $rows :$rows;
+    }
+    
+    
+
+    
+
+    
+   
+    
+}
