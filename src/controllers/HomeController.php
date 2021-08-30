6f3b57d6876b2785ee1068eea8c59537e5d91fed
Vineeth <ipcs022@outlook.com>
2021-08-30 21:26:32 +0530

    serach status updated

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace src\controllers;
 
 use inc\Controller;
+use inc\Raise;
+use src\lib\Router;
+use src\lib\Helper;
+use src\lib\Secure;
+use src\lib\RRedis;
+use src\lib\ValidatorFactory;
 use src\models\Home;
 use src\models\Category;
 
@@ -22,12 +28,18 @@ class HomeController extends Controller
 
      public function actionGetHome(){
 
-        $input        = $_POST; 
-        $search       = issetGet($input,'search_keyword','');
-        $bookDetail   = $this->mdl->getTrending($search);
-        $categoryList = $this->categorymdl->getList();
+        $input         = $_POST; 
+        $search        = issetGet($input,'search_keyword','');
+        $userObj        = Raise::$userObj;
+        $userId         = $userObj['id']; 
+        if(empty($userId)) {
 
-        $data         = ['bookDetail'=>$bookDetail, 'categoryList'=>$categoryList];
+            return $this->renderAPIError('Userid cannot be empty','');  
+        }
+        $bookDetail    = $this->mdl->getTrending($search);
+        $categoryList  = $this->categorymdl->getList();
+        $search_status = empty($search) ? 'false':'true';
+        $data          = ['bookDetail'=>$bookDetail, 'categoryList'=>$categoryList,'search'=>$search_status];
         return $this->renderAPI($data, 'Trending Books and Category List', 'false', '', 'true', 200);
 
     }
